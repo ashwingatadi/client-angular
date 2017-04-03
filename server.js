afb50512ffda283ca35228cc40919bf2bcf4a013
Ashwin Gatadi <ashwin.gatadi@gmail.com>
2017-04-02 19:37:24 -0700

    Express added

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(port,()=>{
+    console.log(`App started on port ${port}`);
+});
